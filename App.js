d9e9719636e7d2582ae4dc20bf0787ea4749a53e
Kishara Manilka <kisharamanilka0@gmail.com>
2024-08-18 11:27:20 +0530

    changes are done to app.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const[enteredGoalText, setEnteredGoalText]= useState('');
+  function goalInputHandler(enteredText){
+    setEnteredGoalText(enteredText);
+  };
+  function addGoalHandler(){
+    console.log.apply(enteredGoalText);
+  };
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!'/>
-        <Button title='Add Goal'/>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder=' Your course goal!'onChangeText={goalInputHandler}/>
+        <Button title='Add Goal'onPress={addGoalHandler}/>
       </View>
       <View>
         <Text>List Of Goals</Text>
@@ -22,4 +29,16 @@ const styles = StyleSheet.create({
   appContainer: {
     padding:50,
   },
+  inputContainer: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    padding:0,
+  },
+  TextInput:{
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'80%',
+    marginRight:8,
+  }
+
 });
